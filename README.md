78cad4cace9a9899b160bb9f97d4404c24ab029f
BlackAsLight <44320105+BlackAsLight@users.noreply.github.com>
2021-09-12 03:49:20 +1000

    v1.1.0

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # PaperLogs
 
-This Node.JS script consolidates logs that happened on the same day.
+This Node.JS program consolidates logs that happened on the same day. Consolidating all the files from the same day into one file saves very little disk space. An insignificant amount really, but does cut down on how many you have to scroll through.
+
+## Instructions
+
+### Setup
+
+1. Download this program onto the computer you wish to use it on.
+2. Open up a terminal and direct yourself to the downloaded folder's contents using `cd`.
+3. Type `npm run build` to install the necessary dependencies and compile the program.
+
+### Run
+
+ - Type `node main.js <path>` to execute the program.
+ - Replace `<path>` with the path to the folder that you'd like this program to run on.
+ - You can do multiple `<path>` after `node main.js` if there are multiple folders you wish to have consolidated.
+  - Example: `node main.js <path1> <path2> <path3>`
--- main.ts
+++ main.ts
@@ -1,1 +1,71 @@
-console.log('Hello World!');
+import * as fs from 'fs';
+import * as zlib from 'zlib';
+
+let args = process.argv;
+args.shift();
+args.shift();
+console.log(`${args.length} Path${args.length == 1 ? 's' : ''} Provided.`);
+(async () => {
+	while (args.length) {
+		const path = (() => {
+			const arg = args.shift();
+			if (arg.endsWith('/'))
+				return arg;
+			return arg + '/';
+		})();
+		if (!fs.existsSync(path)) {
+			console.log(`Invalid Path: ${path}`);
+			continue;
+		}
+
+		let files = fs.readdirSync(path).filter(x => x.endsWith('.log.gz')).sort();
+		console.log(`Found ${files.length} logs.`);
+		let days = {};
+
+		const yesterday = new Date().getTime() - 86400000;
+		while (files.length) {
+			const file = files.shift();
+			const date = file.slice(0, 10);
+			if (new Date(date).getTime() < yesterday) {
+				if (days[date] == null)
+					days[date] = [];
+				days[date].push(file);
+			}
+		}
+		console.log(`Logs sorted into ${Object.keys(days).length} days.`);
+
+		for (const day in days) {
+			const count = days[day].length;
+			console.log(`Day, ${day}, has ${count} logs.${count == 1 ? ' Skipping...' : ''}`);
+			if (count == 1)
+				continue;
+
+			const writeStream = fs.createWriteStream(path + day + '.log');
+			let hadError = false;
+			for (const file of days[day]) {
+				const response: boolean = await new Promise((resolve, reject) => fs.createReadStream(path + file)
+					.pipe(zlib.createGunzip())
+					.on('data', (chunk) => writeStream.write(chunk))
+					.on('close', () => resolve(true))
+					.on('error', () => reject(false)));
+				if (!response) {
+					console.log(`An unknown error occurred while processing file ${file}. Scraping Progress made for that day and moving on.`);
+					writeStream.close();
+					fs.rmSync(path + day + '.log');
+					hadError = true;
+					break;;
+				}
+			}
+
+			if (!hadError) {
+				for (const file of days[day])
+					fs.rmSync(path + file);
+				writeStream.close();
+				const compressedStream = fs.createReadStream(path + day + '.log').pipe(zlib.createGzip()).pipe(fs.createWriteStream(path + day + '.log.gz'));
+				compressedStream.on('close', () => {
+					fs.rmSync(path + day + '.log');
+				});
+			}
+		}
+	}
+})();
